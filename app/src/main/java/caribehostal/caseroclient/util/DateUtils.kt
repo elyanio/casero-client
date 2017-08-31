d5ae80a396748123d2b57c8674a4dbb42ab54283
Rainer Martínez Fraga <rainermf@polymitasoft.com>
2017-08-31 17:57:41 -0400

    Supress deprecation in date conversions

--- app/src/main/java/caribehostal/caseroclient/util/DateUtils.kt
+++ app/src/main/java/caribehostal/caseroclient/util/DateUtils.kt
@@ -5,10 +5,12 @@ package caribehostal.caseroclient.util
 import org.threeten.bp.LocalDate
 import java.util.*
 
+@Suppress("DEPRECATION")
 fun Date.toLocalDate(): LocalDate {
     return LocalDate.of(this.year + 1900, this.month + 1, this.date)
 }
 
+@Suppress("DEPRECATION")
 fun LocalDate.toDate(): Date {
     return Date(this.year - 1900, this.monthValue - 1, this.dayOfMonth)
 }
