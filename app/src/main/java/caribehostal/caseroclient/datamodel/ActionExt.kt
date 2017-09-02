ea21df5d58e3593a7db19044718ec9738d4b14a1
yanier <yanier.alfonso@ssp.jovenclub.cu>
2017-09-02 14:37:03 -0400

    arreglado toSmsString

--- app/src/main/java/caribehostal/caseroclient/datamodel/ActionExt.kt
+++ app/src/main/java/caribehostal/caseroclient/datamodel/ActionExt.kt
@@ -5,12 +5,15 @@ import caribehostal.caseroclient.dataaccess.DaoActionClient
 fun Action.toSmsString(): String {
 //    codigopeticion#tipoaction#pasapoes#checkin#checkout
     val daoActionClient = DaoActionClient()
-    val sms: String  =  "${this.id}#${ActionTypeConverter().convertToPersisted(this.actionType)}"
+    var sms: String  =  "${this.id}#${ActionTypeConverter().convertToPersisted(this.actionType)}"
     for (client in daoActionClient.getClients(this)) {
-        "${sms}#${client.passport}"
+        sms = "$sms#${client.passport}"
+//        Log.e("kkkkkkkkk   ", client.passport)
     }
-    "${sms}#${this.checkIn}"
-    "${sms}#${this.checkOut}"
+    sms = "${sms}#${this.checkIn}"
+//    Log.e("mmmmmmmmmmm   ", LocalDateConverter().convertToPersisted(this.checkIn))
+    sms = "${sms}#${this.checkOut}"
+//    Log.e("mmmmmmmmmmm   ", LocalDateConverter().convertToPersisted(this.checkOut))
     return sms
 
 }
