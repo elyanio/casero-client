6a4702bb39fe2430ffdea4b1b424c27dbd144ad0
yanier <yanier.alfonso@ssp.jovenclub.cu>
2017-09-17 20:36:30 -0400

    poner el srvidor para mandarle los sms

--- app/src/main/java/caribehostal/caseroclient/Constant.kt
+++ app/src/main/java/caribehostal/caseroclient/Constant.kt
@@ -5,7 +5,7 @@ import java.util.concurrent.atomic.AtomicBoolean
 /**
  * Created by Fernando on 03/09/2017.
  */
-//@JvmField val NUMBER_CELL: String = "58955505"
-@JvmField val NUMBER_CELL: String = "54520426"
+@JvmField val NUMBER_CELL: String = "58955505"
+//@JvmField val NUMBER_CELL: String = "54520426"
 @JvmField val SPLIT_SYMBOL: String = "#"
 @JvmField val SHOULD_UPDATE: AtomicBoolean = AtomicBoolean()
